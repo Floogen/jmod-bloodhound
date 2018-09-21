8ac193dfb8ea70b029350d7f6fc963a0bb57a3ee
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2018-09-21 11:37:56 -0500

    Creation of python script, reorganizing of folders

--- JMOD_Bloodhound Script - Python/JMOD_Sniffer.py
+++ JMOD_Bloodhound Script - Python/JMOD_Sniffer.py
@@ -1,7 +1,33 @@
 import praw
+import time
+
+
+def find_jmod_comments(post):
+    # return list of comments (if any) for given post
+    comment_list = []
+
+    while True:
+        try:
+            post.comments.replace_more(limit=0)
+            break
+        except Exception:
+            print('Handling replace_more exception')
+            time.sleep(1)
+
+    for comment in post.comments.list():
+        if comment.author_flair_css_class == "jagexmod" or comment.author_flair_css_class == "modmatk" \
+                or comment.author_flair_css_class == "mod-jagex":
+            comment_list.append(comment)
+
+    return comment_list
+
 
 reddit = praw.Reddit('JMOD_Bloodhound', user_agent='User Agent - JMOD_Bloodhound PS Script')
 
-subreddit = reddit.subreddit('redditdev')
-print(subreddit.display_name)
-print(subreddit.title)
+subreddit = reddit.subreddit('2007scape')
+
+for submission in subreddit.hot(limit=1):
+    print(submission.title)
+    jmod_list = (find_jmod_comments(submission))
+    if len(jmod_list) > 0:
+        print("JMOD commends be ere mateys")
